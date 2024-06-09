75a1fc505c9ecc4e2a337bd1a281c0fd523c3166
kbeachler <kaleigh8b@gmail.com>
2024-06-09 10:00:25 -0700

    completed project tasks, added material and functional component - button and header, and tested

---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = ({ title, subtitle }) => (
+  <div style={{ textAlign: 'center', padding: '20px', color: 'white' }}>
+    <h1>{title}</h1>
+  </div>
+);
+
+export default Header;
--- src/component/todos.js
+++ src/component/todos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, CardContent, Grid, ListItemButton, ListItemText, Checkbox } from "@mui/material";
 import "../component/todos.css";
 
 // 1. This component formats and returns the list of todos.
@@ -13,23 +14,23 @@ const Todos = ({ todos }) => {
       todos.map((todo) => {
         return (
           <Grid key={todo.id}>
-            <Card>
+             <Card style={{ backgroundColor: "#111E33" }}>
               {/* Remember, we set the local state of this todo item when the user submits the form in 
               AddTodo.js. All we need to do is return the todo list item */}
               <CardContent>
-                <span style={{ padding: "50px" }}>add todo content</span>
+                <span style={{ padding: "50px", color: "white"}}>{todo.content}</span>
               </CardContent>
             </Card>
           </Grid>
         );
       })
     ) : (
-      <p>{/*Enter your message here if todo list is empty*/}</p>
+      <p>{"You have no todo's left"}</p>
     );
     // Lastly, return the todoList constant that we created above to show all of the items on the screen.
     return (
       <div className="todoCollection" style={{ padding: "10px" }}>
-        {/*call todo list here*/}
+        {todoList}
       </div>
     );
 };

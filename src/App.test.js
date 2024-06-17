3d0f01974a1b15cb363777e13e99b33ea2dc1749
kbeachler <kaleigh8b@gmail.com>
2024-06-17 01:33:32 -0700

    finished last merge conflicts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { render, screen, fireEvent, waitFor} from '@testing-library/react';
-=======
 import { render, screen } from '@testing-library/react';
->>>>>>> main
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
@@ -22,11 +18,7 @@ afterEach(() => {
 
 test('test that App component renders', () => {
   render(<App />, container);
-<<<<<<< HEAD
-});
-=======
  });
->>>>>>> main
 
 test('test that new-item-button is a button', () => {
   render(<App/>, container);
@@ -39,60 +31,3 @@ test('test that new-item-textfield is an textfield ', () => {
   const element = screen.getByTestId('new-item-textfield');
   expect(element).toBeInTheDocument();
 });
-<<<<<<< HEAD
-
-test('renders Todos component with empty todo list', () => {
-  render(<App />, container);
-  expect(screen.getByText("You have no todo's left")).toBeInTheDocument();
-});
-
-test('test that App component doesn\'t add a blank task', () => {
-  render(<App />, container);
-  const addButton = screen.getByTestId('new-item-button');
-
-  fireEvent.click(addButton);
-
-  const check = screen.getByText(/You have no todo's left/i);
-  expect(check).toBeInTheDocument();
-});
-
-
-test('test that App component renders Task', () => {
-  render(<App />, container);
-  const taskInput = screen.getByTestId("new-item-textfield");
-  const addButton = screen.getByTestId('new-item-button');
-
-  fireEvent.change(taskInput.querySelector('input'), { target: { value: 'History Test' } });
-
-  waitFor(() => {
-    expect(taskInput.value).toBe("History Test");
-  });
-
-  fireEvent.click(addButton);
-
-  waitFor(() => {
-    const taskTextElement = screen.getByText(/History Test/i);
-    expect(taskTextElement).toBeInTheDocument();
-  });
-});
-
-test('test that App component renders Task', () => {
-  render(<App />, container);
-  const taskInput = screen.getByTestId("new-item-textfield");
-  const addButton = screen.getByTestId('new-item-button');
-
-  fireEvent.change(taskInput.querySelector('input'), { target: { value: 'History Test' } });
-
-  waitFor(() => {
-    expect(taskInput.value).toBe("History Test");
-  });
-
-  fireEvent.click(addButton);
-
-  waitFor(() => {
-    const taskTextElement = screen.getAllByText(/History Test/i);
-    expect(taskTextElement.length).toBe(1);
-  });
-});
-=======
->>>>>>> main
